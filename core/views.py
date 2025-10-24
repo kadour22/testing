1244ee7daacdf997e6e01f3eb7bbc1396bc37293
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-10-25 00:37:11 +0100

    add drf

--- core/views.py
+++ core/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.renderers import TemplateHTMLRenderer
 
 def index(request):
     return render(request, "index.html")
+
+
+class index_api_view(APIView) :
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = "index.html"
+
+    # def get(self, request):
+    #     return
